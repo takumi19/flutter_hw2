993eb5eb7fd546f847308ff550d675d1e0c7c42f
System Administrator <root@Maxs-Laptop.local>
2024-03-13 19:20:34 +0300

    Formatted and added screenshots to the Screenshots folder

--- lib/presentation/bloc/favorite/favorite_event.dart
+++ lib/presentation/bloc/favorite/favorite_event.dart
@@ -1,45 +1,26 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
-
 part of 'favorite_bloc.dart';
 
-
 sealed class FavoriteEvent extends Equatable {
-
   const FavoriteEvent();
 
-
   @override
-
   List<Object> get props => [];
-
 }
-
 
 class AddToFavorite extends FavoriteEvent {
-
   final ArticleEntity article;
-
 
   const AddToFavorite({
-
     required this.article,
-
   });
-
 }
-
 
 class RemoveFromFavorite extends FavoriteEvent {
-
   final ArticleEntity article;
 
-
   const RemoveFromFavorite({
-
     required this.article,
-
   });
-
 }
-
--- lib/presentation/bloc/theme/theme_event.dart
+++ lib/presentation/bloc/theme/theme_event.dart
@@ -1,19 +1,12 @@
 part of 'theme_bloc.dart';
 
-
 class ThemeEvent extends Equatable {
-
   const ThemeEvent();
 
-
   @override
-
   List<Object> get props => [];
-
 }
 
-
 class SwitchToDark extends ThemeEvent {}
-
 
 class SwitchToLight extends ThemeEvent {}
